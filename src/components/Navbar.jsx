eb26bf0d6307cbe9e2eb03b41234e053c78772af
suryansh <suryanshgoel7017@gmail.com>
2026-07-13 14:21:54 +0530

    Add Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import "./Navbar.css";
+function Navbar(){
+  return (
+    <nav>
+      <h2>Myntra clone</h2>
+      <ul>
+        <li>Men </li>
+        <li>Women</li>
+        <li>Kids</li>
+        <li>Home</li>
+        <li>Beauty</li>
+        
+
+      </ul>
+    </nav>
+  );
+
+}
+export default Navbar;
